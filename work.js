52a1555aaf31566490250cf506fbc5097e217aaa
sho ebisui <raphi.jus1412@gmail.com>
2022-09-12 17:41:20 +0900

    ワーク追加

--- work.js
+++ work.js
@@ -1,27 +1,33 @@
-let points = [a, b, c, d, e]
-let number = points.length
+let points = [55,65,70];
+let number = points.length;
 
-function get_achievement(a, b, c, d, e){
-  let sum = a + b + c + d + e;
-  if (sum > 400){
-   return "A";
-  } elseif (sum > 300){
-    return "B";
-  } elseif (sum > 250){
-    return "C";
-  } else {
-    return "D";
-  }
-};
-
-console.log()
-
-function get_pass_or_failure {
+function get_pass_or_failure(points) {
   let judge = "合格";
   for(let i=0; i<number; i++){
     if(points[i]<60){
       judge = "不合格";
-      break;
     }
   }
-}
+  return judge;
+};
+
+console.log(get_pass_or_failure(points));
+
+function get_achievement(points){
+  let sum = 0;
+  for(let i=0; i<number;i++){
+    sum += points[i];
+  }
+
+  if(sum >= 250){
+    return "A";
+  } else if(sum >= 200){
+    return "B";
+  }else if(sum >= 150){
+    return "C";
+  }else{
+    return "D";
+  }  
+};
+
+console.log(get_achievement(points));
